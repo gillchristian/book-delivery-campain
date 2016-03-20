0df6152b1e93e1790be6425b9ea5efc274f3fec8
gillchristian <gillchristiang@gmail.com>
2016-03-20 17:18:40 -0300

    Simple gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+
+'use strict';
+   // this is strict...
+
+var gulp            = require('gulp'),
+    notify          = require('gulp-notify'),
+    // CSS plugins
+    rucksack        = require('gulp-rucksack'),
+    sass            = require('gulp-sass');
+
+// import the variables for the tasks and files, etc
+
+var TASKS           = require('./gulp-helpers/tasks'),
+    FILES           = require('./gulp-helpers/files');
+
+// deveolpment style tasks
+// --------------------------------------------------------
+gulp.task(TASKS.dev.style, () => {
+  return gulp.src(FILES.css.source)
+    .pipe( sass().on('error' , err => {
+      notify({title: 'CSS Task'}).write(err.line  + ': ' + err.message);
+      return this.emit('end')
+    } ))
+    .pipe( rucksack({autoprefixer: true, fallbacks: false }) )
+    .pipe( gulp.dest(FILES.css.dest) );
+});
+
+gulp.task(TASKS.watch, () => {
+    gulp.watch( FILES.css.all , [TASKS.dev.style]);
+});
+
+// default
+// --------------------------------------------------------
+gulp.task(TASKS.default, [TASKS.dev.style, TASKS.watch] );
